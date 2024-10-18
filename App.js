2c7a40fdc0c1362ee02a773dbf629d297d0442a7
Gauravcodeit <adhikarigaurav82@gmail.com>
2024-10-19 00:16:36 +0530

    playing with setTimeout

--- App.js
+++ App.js
@@ -1,4 +1,32 @@
 console.log("a");
-require("./xyz");
+// require("./xyz");
+//  MODULE TYPE  PACKAGE JSON
+// import { calculate } from "./sum.js";
 const {calculate} = require("./sum");
 calculate(5,5)
+setTimeout(()=>{
+    console.log("zero second")
+}, 0)
+function var1(){
+    return "var1"
+}
+function var2 (){
+    return ("var 2" + var1())
+}
+console.log(var2())
+console.log(var1())
+function var3(){
+    console.log("var3")
+    setTimeout(()=>{
+        console.log("settime inside function zero second")
+    },0)
+}
+var3()
+function var4 (){
+    return ("var 2" + var1())
+}
+console.log(var4())
+
+for ( let i = 0;  i< 1000; i++ ){
+    if (i % 100 == 0 ) console.log("win", i)
+}
--- sum.js
+++ sum.js
@@ -2,12 +2,38 @@ var b = "Hello";
 function calculate(a, b){
     console.log(a + b)
 }
+// z= "Hello";
+// console.log(z)
+//  MODULE TYPE  PACKAGE JSON
+
+// export function calculate(a, b){
+//     console.log(a + b)
+// }
 calculate(1,3);
+// module.exports = {
+//     b: b,
+//     calculate: calculate
+// }
 module.exports = {
-    b: b,
-    calculate: calculate
+     b,
+     calculate
 }
 // module.exports = {
 //     b,
 //     calculate
 // }
+
+
+
+
+
+// {
+//     "type" : "commonjs"
+// }
+// {
+//     "type" : "module"
+// }
+
+setTimeout(()=>{
+console.log("I'm in the sum file")
+}, 0)
