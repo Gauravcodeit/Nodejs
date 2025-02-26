c51505c2eded8e8384a7a9ee467152e1665be03c
Gauravcodeit <adhikarigaurav82@gmail.com>
2025-02-26 21:17:51 +0530

    updated the code with express js

--- App.js
+++ App.js
@@ -1,57 +1,12 @@
-console.log("a");
-const fs = require("fs");
-const https = require("https");
-const http = require("http");
-const server =http.createServer((req, res)=>{
-
-   if (req.url=== "/homepage" ){
-        res.end("homepage")
-   }
-   else{
-    res.end("main server")
-   }
-   console.log("server")
-
+const  express=  require('express');
+const app =express();
+const port = 3000;
+app.get('/', (req, res)=>{
+    res.send('Hello from express')
 })
-server.listen(5500, ()=>{
-    console.log("server is running on localhost")
+app.get('/homes', (req, res)=>{
+    res.send('Hello from express Home')
 })
-
-https.get('https://dummyjson.com/products/1', (res)=>{
-    console.log("result from http request")
+app.listen(port, ()=>{
+    console.log(`express server is running on port ${port}`)
 })
-fs.readFile("./file.txt", "utf8", (err, data)=>{
-    console.log("file data", data)
-})
-
-// require("./xyz");
-//  MODULE TYPE  PACKAGE JSON
-// import { calculate } from "./sum.js";
-const {calculate} = require("./sum");
-calculate(5,5)
-setTimeout(()=>{
-    console.log("zero second")
-}, 0)
-function var1(){
-    return "var1"
-}
-function var2 (){
-    return ("var 2" + var1())
-}
-console.log(var2())
-console.log(var1())
-function var3(){
-    console.log("var3")
-    setTimeout(()=>{
-        console.log("settime inside function zero second")
-    },0)
-}
-var3()
-function var4 (){
-    return ("var 2" + var1())
-}
-console.log(var4())
-
-for ( let i = 0;  i< 1000; i++ ){
-    if (i % 100 == 0 ) console.log("win", i)
-}
