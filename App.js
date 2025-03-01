0b664ba35a13c6d7c1c9efbf573bf168e4d5f7cf
Gauravcodeit <adhikarigaurav82@gmail.com>
2025-03-01 18:16:39 +0530

    basic use of express server

--- App.js
+++ App.js
@@ -3,9 +3,19 @@ const app =express();
 const port = 3000;
 app.get('/', (req, res)=>{
     res.send('Hello from express')
+    // overide all other /dash, /home etc..
 })
 app.get('/homes', (req, res)=>{
     res.send('Hello from express Home')
+})
+app.use('/', (req, res)=>{
+    res.send('Hello from express /')
+})
+app.use('/dash', (req, res)=>{
+    res.send('Hello from express DASH')
+})
+app.use('/dashw', (req, res)=>{
+    res.send('Hello from express DASHw')
 })
 app.listen(port, ()=>{
     console.log(`express server is running on port ${port}`)
