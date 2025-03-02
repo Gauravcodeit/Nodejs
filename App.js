b4aec826a716c64d91b8b07f44fd3943f4772cf5
Gauravcodeit <adhikarigaurav82@gmail.com>
2025-03-02 10:53:35 +0530

    controller and routers

--- App.js
+++ App.js
@@ -1,22 +1,38 @@
 const  express=  require('express');
 const app =express();
 const port = 3000;
-app.get('/', (req, res)=>{
-    res.send('Hello from express')
-    // overide all other /dash, /home etc..
+// app.get('/', (req, res)=>{
+//     res.send('Hello from express')
+//     // overide all other /dash, /home etc..
+// })
+app.get('/test', (req, res)=>{
+    console.log(req.query)
+    res.send({firstname: "Gaurav", lastname: "Adhikari"})
 })
-app.get('/homes', (req, res)=>{
-    res.send('Hello from express Home')
+app.get('/ab+', (req, res)=>{
+    res.send({usecase:"ab+"})
 })
-app.use('/', (req, res)=>{
-    res.send('Hello from express /')
+
+app.get('/ab+c', (req, res)=>{
+    res.send({usecase:"ab+c" })
 })
-app.use('/dash', (req, res)=>{
-    res.send('Hello from express DASH')
+app.delete('/test', (req, res)=>{
+
+    res.send('Successfully deleted the data')
 })
-app.use('/dashw', (req, res)=>{
-    res.send('Hello from express DASHw')
+app.post('/test', (req, res)=>{
+    res.send('Successfully saved the data')
 })
+
+app.get('/user/:id/:pass',(req, res)=>{
+    console.log(req.params, "second")
+    res.send(req.params)
+})
+app.get('/user/:id',(req, res)=>{
+    console.log(req.params, "one")
+    res.send(req.params)
+})
+
 app.listen(port, ()=>{
     console.log(`express server is running on port ${port}`)
 })
